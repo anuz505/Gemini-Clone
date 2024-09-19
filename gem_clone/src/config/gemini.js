99e16fc3d1d118b8c2a2bfca79b515dfa5e65b61
anuj bhandari <anujb364@gmail.com>
2024-09-19 12:20:26 +0545

    Refactor gemini.js configuration file

--- gem_clone/src/config/gemini.js
+++ gem_clone/src/config/gemini.js
@@ -3,15 +3,16 @@
  *
  * $ npm install @google/generative-ai
  */
-import dotenv from "dotenv";
-dotenv.config({ path: "../.env" });
+
 import {
   GoogleGenerativeAI,
   HarmCategory,
   HarmBlockThreshold,
 } from "@google/generative-ai";
+import dotenv from "dotenv";
+dotenv.config;
 
-const apiKey = import.meta.VITE_GEMINI_API_KEY;
+const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
 
 const genAI = new GoogleGenerativeAI(apiKey);
 
